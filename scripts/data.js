aaafeb3a9344380e6535dc71626ba62afb3b4922
nutbrain <332928791@qq.com>
2019-10-10 17:12:02 +0800

    modify data.js

--- scripts/data.js
+++ scripts/data.js
@@ -2,5 +2,9 @@ var webs=[
     {
         title:"x-mol",
         url:"https://www.x-mol.com/",
-    }
+    },
+    {
+        title:"bootstrap中文网",
+        url:"https://www.bootcss.com/",
+    },
 ]
